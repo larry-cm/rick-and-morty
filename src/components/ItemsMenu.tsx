563aff89109577d0b9aa557d9d4dfc075180cd59
larry1sf <larryycel@gmail.com>
2025-02-15 03:51:55 -0500

    white mobile

--- src/components/ItemsMenu.tsx
+++ src/components/ItemsMenu.tsx
@@ -1,28 +1,34 @@
 import { Persons, Ubicaciones, Temporadas } from "@/icons/menuIcons"
+import type { ComponentChildren } from "preact"
 
+function MiniItem({ children, ruta }: { children: ComponentChildren, ruta: string }) {
+  return (
+    <a href={ruta ?? "personajes"}
+      class="flex flex-row gap-1 items-center text-white hover:bg-gray-600/20 p-2 rounded-lg min-w-max text-sm"
+    >
+
+      {children}
+    </a>
+  )
+}
 export default function ItemsMenu() {
   return (
     <>
-      <a href="personajes"
-        class="flex flex-row gap-1 items-center  hover:bg-gray-600/20 p-2 rounded-lg min-w-max text-sm"
-      >
+
+      <MiniItem ruta="personaje">
         <Persons />
         <p>Personajes</p>
-      </a>
+      </MiniItem>
 
-      <a href="#"
-        class="flex flex-row gap-1 items-center hover:bg-gray-600/20 p-2 rounded-lg min-w-max text-sm"
-      >
+      <MiniItem ruta="#">
         <Temporadas />
         <p>Temporadas</p>
-      </a>
-
-      <a href="#"
-        class="flex flex-row gap-1 items-center hover:bg-gray-600/20 p-2 rounded-lg min-w-max text-sm"
-      >
+      </MiniItem>
+      <MiniItem ruta="#">
         <Ubicaciones />
         <p>Ubicaciones</p>
-      </a>
+      </MiniItem>
+
     </>
   )
 }
